34811e3bb2e4df5764867fc03f9a519b3dfe5782
Sergey Parshukov <sergey.parshukov@me.com>
2014-06-18 23:45:41 +0400

    Initial Commit

---
+++ EcoClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EcoClient
+//
+//  Created by Sergey Parshukov on 18.06.14.
+//  Copyright (c) 2014 bugman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
